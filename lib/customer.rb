6d2c0fe61edd39e424a9acbc15516450d68f0799
Brandon Dusch <brandondusch@gmail.com>
2020-03-16 03:45:48 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,14 @@
 class Customer
+  @@all = []
   attr_accessor :name, :age 
   
   def initialize(name, age) 
     @name = name 
     @age = age 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
   end
 end
