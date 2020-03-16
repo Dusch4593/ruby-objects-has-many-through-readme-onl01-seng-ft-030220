4e3790dde612d107561f45760489224d612f3d75
Brandon Dusch <brandondusch@gmail.com>
2020-03-16 03:47:30 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,13 @@
 class Waiter
+  @@all = []
   attr_accessor :name, :years_of_experience 
   
   def initialize(name, years_of_experience)
     @name = name 
     @years_of_experience = years_of_experience 
+  end
+  
+  def self.all 
+    @@all 
   end
 end
